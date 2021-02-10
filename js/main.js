74dac2feb5448c6e9033c9d1013cf6d5d59cdbac
rodrisoares <diihhsoares2@gmail.com>
2021-02-10 11:13:10 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//My api key
+var apikey = {
+    key: 'a91ee552-ba37-48d9-a45c-7c0bc1070935'
+}
+
+//GET Fetch Requisition
+fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' +
+    apikey.key)
+    .then((response) => {
+        if(!response.ok) throw new Error('Erro ao executar a requisição, status ' + response.status);
+        return response.json();
+})
+.then((api) => {
+    
+    var texto = "";
+    // Get 10 coins and symbols 
+    for(let i = 0; i < 10; i++){
+
+    //Show API information
+       texto = texto + `
+        <div class="media">
+            <img src="img/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+            <div class="media-body">
+            <h5 class="mt-2">${api.data[i].name}</h5>
+            <p>${api.data[i].symbol}</p>
+            <p>${api.data[i].first_historical_data}</p>
+            </div>
+        </div> `;
+
+        document.getElementById("coins").innerHTML = texto;
+    }
+})
+.catch((error) => {
+    console.error(error.message);
+});
